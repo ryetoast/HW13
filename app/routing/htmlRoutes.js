11e3181b96a8a0c151a7383451a0aefa33ad2727
Ryan <ryetoaster@gmail.com>
2018-04-24 22:17:58 -0700

    push

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,6 @@
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "view.html"));
+});
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "view.html"));
+});
